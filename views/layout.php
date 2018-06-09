7ba315cefc57ef9303cae72c40690dede5c1bee3
isaacjs <isaacseijas7@gmail.com>
2018-06-09 13:23:00 -0400

    restructurando reestructurando la carpeta view

--- views/layout.php
+++ views/layout.php
@@ -1,13 +1,19 @@
-<!doctype html>
-<html lang="es">
+<!DOCTYPE html>
+<html lang="es-VE">
 <head>
-    <meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta name="author" content="Carlos, Isaac, Angel">
+	<meta name="description" content="Maraton UNERG 2018">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+	<title>Maraton UNERG 2018</title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/main.css">
 </head>
 <body>
+    
     <?php echo $templateContent; ?>
+
+	<script src="../../public/js/main.js"></script>
 </body>
 </html>
