00fef86fa5bcc23cca2780422a0afa50af61b3a9
isaacjs <isaacseijas7@gmail.com>
2018-06-09 13:08:42 -0400

    GRIBS

---
+++ src/views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-VE">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Carlos, Isaac, Angel">
+	<meta name="description" content="Maraton UNERG 2018">
+	<title>Maraton UNERG 2018</title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/main.css">
+</head>
+<body>
+	<script src="../../public/js/main.js"></script>
+</body>
+</html>
---
+++ src/views/welcome.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="es-VE">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Carlos, Isaac, Angel">
+	<meta name="description" content="Maraton UNERG 2018">
+	<title>Maraton UNERG 2018</title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/main.css">
+</head>
+<body>
+	
+	<div class="row">
+		<div class="col-1">
+			<div class="example">
+				col-1
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-2">
+			<div class="example">
+				col-2
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-3">
+			<div class="example">
+				col-3
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-4">
+			<div class="example">
+				col-4
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-5">
+			<div class="example">
+				col-5
+			</div>
+		</div>
+	</div>
+
+	<hr>
+
+	<div class="row">
+		<div class="col-1">
+			<div class="example">
+				col-1
+			</div>
+		</div>
+		
+		<div class="col-4">
+			<div class="example">
+				col-4
+			</div>
+		</div>
+	</div>
+
+	<hr>
+
+	<div class="row">
+		<div class="col-2">
+			<div class="example">
+				col-2
+			</div>
+		</div>
+		
+		<div class="col-3">
+			<div class="example">
+				col-3
+			</div>
+		</div>
+	</div>
+
+	<hr>
+
+	<div class="row">
+		<div class="col-2">
+			<div class="example">
+				col-2
+			</div>
+		</div>
+
+		<div class="col-2">
+			<div class="example">
+				col-2
+			</div>
+		</div>
+		
+		<div class="col-1">
+			<div class="example">
+				col-1
+			</div>
+		</div>
+	</div>
+
+	<script src="../../public/js/main.js"></script>
+</body>
+</html>
